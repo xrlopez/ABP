84a9402251f2497bb9cb12e5d2a1bd760f5896af
xrlopez <r.xeila27@gmail.com>
2015-11-20 17:00:53 +0100

    perfil jpop

--- view/juradoPopular/perfil.php
+++ view/juradoPopular/perfil.php
@@ -15,8 +15,9 @@
 <div class="row registrarE">
 <div class="divLogin col-xs-12 col-sm-12 col-md-12">
     <h2>Jurado Popular</h2>
-	    <div>
-		    <div class="row consultarInfo">
+    <form id="form-aceptar" method="POST" action="index.php?controller=juradoPopular&amp;action=eliminar">
+		<div>
+    	    <div class="row consultarInfo">
 		    	<div class="col-xs-4 col-sm-4 col-md-4 info">
 			    	Usuario:  
 		    	</div>
@@ -51,6 +52,9 @@
 	    </div>
 	    <div class="divFormulario">
 	    	<p id="bot"><a href="index.php?controller=juradoPopular&amp;action=modificar">Modificar</a></p>
+			<p id="bot"><a href="#" onclick="if (confirm('estas seguro?')) {document.getElementById('form-aceptar').submit()}">Eliminar</a></p>
+		 	
 		</div>
+    </form>
     </div>
 </div>
