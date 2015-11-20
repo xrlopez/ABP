a71664d83505a22208a7fc95df745fdc3e15b0dd
Daniel <plldani@gmail.com>
2015-11-20 11:14:52 +0100

    Update PinchoMapper.php

--- model/PinchoMapper.php
+++ model/PinchoMapper.php
@@ -4,5 +4,47 @@ require_once(__DIR__."/../model/Pincho.php");
 
 class PinchoMapper {
 
+  private $db;
+  
+  public function __construct() {
+    $this->db = PDOConnection::getInstance();
+  }
+  
+  public static function getNumPinchos(){
+    $query = $this->db->query('SELECT count(*) as num FROM pincho');
+    $num_pinchos = $query->fetch(PDO::FETCH_ASSOC);
+    return $num_pinchos;
+  }
+  
+  public static function getPinchos($inicio = 0, $limite = 5){
+    $list = [];
+    $req = $this->db->query('SELECT * FROM pincho limit '.$inicio.', '.$limite.'');
+    foreach($req->fetchAll() as $pincho) {
+      $list[] = new Pincho($pincho['nombre'], $pincho['celiaco'], $pincho['descripcion'], $pincho['num_votos'], $pincho['id_pincho']);
+    }
+    return $list;
+  }
+  
+  
+  public static function all() {
+    $list = [];
+    $req = $this->db->query('SELECT * FROM pincho');
+    
+    // we create a list of Post objects from the database results
+    foreach($req->fetchAll() as $pincho) {
+      $list[] = new Pincho($pincho['nombre'], $pincho['celiaco'], $pincho['descripcion'], $pincho['num_votos'], $pincho['id_pincho']);
+    }
+    return $list;
+  }
+  
+  public static function find($id) {
+    // we make sure $id is an integer
+    $id = intval($id);
+    $req = $this->db->prepare('SELECT * FROM pincho WHERE id_pincho = '.$id);
+    $req->execute(array());
+    $pincho = $req->fetch();
+    return new Pincho($pincho['nombre'], $pincho['celiaco'], $pincho['descripcion'], $pincho['num_votos'], $pincho['id_pincho']);
+  }
+  
 }
 ?>
