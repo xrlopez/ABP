c191728ba649ed7fdea76933f6750f2efb872545
Daniel <plldani@gmail.com>
2015-11-21 21:08:18 +0100

    Update asignar.php

--- view/organizador/asignar.php
+++ view/organizador/asignar.php
@@ -5,22 +5,20 @@ $jurados = $view->getVariable("juradosProfesionales");
 
 
 ?>
-			<div class="row">
-				<div id="main" class="col-xs-20 col-sm-12 col-md-8">
-					<div class="row registrarE">
-						<div class="divLogin col-xs-12 col-sm-12 col-md-12">
-							<h2>Selecciona un miembro del Jurado Profesional</h2>
-								<form id="asignarPincho" action="index.php?controller=organizador&amp;action=asignarJurado" method="post" >
-									<?php
-									foreach ($jurados as $jurado) {
-									?>	
-										<p><input type="radio" name="usuario" id="usuario" value="<?= $jurado->getId()?>" checked><?php echo $jurado->getNombre() ?></p>
-									<?php
-									}
-									?>
-									<p id="bot"><input name="submit" type="submit" id="boton" value="Elegir" class="boton"/></p>
-				                </form>
-						</div>
-					</div>
-				</div>
-			</div>
+
+<div class="row registrarE">
+	<div class="divLogin col-xs-12 col-sm-12 col-md-12">
+		<h2>Selecciona un miembro del Jurado Profesional</h2>
+			<form id="asignarPincho" action="index.php?controller=organizador&amp;action=asignarJurado" method="post" >
+				<?php
+				foreach ($jurados as $jurado) {
+				?>	
+					<p><input type="radio" name="usuario" id="usuario" value="<?= $jurado->getId()?>" checked><?php echo $jurado->getNombre() ?></p>
+				<?php
+				}
+				?>
+				<p id="bot"><input name="submit" type="submit" id="boton" value="Elegir" class="boton"/></p>
+        </form>
+	</div>
+</div>
+
